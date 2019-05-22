f19b99568cf95f30f519a3d876ab923ca5158f9f
Victoria Chen <taichen86@gmail.com>
2019-05-22 12:29:30 +0200

    show game view grid and players

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,6 +1,53 @@
-function getListOfGames(){
-    console.log( "getting list of games... " );
-    fetch("/api/games" ).then( function(response){
+// function getListOfGames(){
+//     console.log( "getting list of games... " );
+//     fetch("/api/games" ).then( function(response){
+//         console.log( "got response: " + response );
+//         if( response.ok ){
+//             return response.json();
+//         }
+
+//     } ).then( function( json ){
+//         console.log( "got json: " + json );
+//          var data = json;
+//         console.log( data );
+//         data.forEach( game => {
+//            console.log( game );
+//            vm.games.push( game );
+//         });
+
+
+//     console.log( vm.games );
+//     }).catch( function( error ){
+//         console.log( "error: " + error );
+//     })
+// }
+// getListOfGames();
+
+var vm = new Vue({
+    el: '#vue-instance',
+    data: {
+        games: [],
+        rows: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        columns: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        gameplayerID: 0,
+        gameplayers: []
+    }
+})
+
+getGameView();
+
+function getGameView()
+{
+    console.log( location.search );
+    console.log( paramObj( location.search ))
+    const gp = paramObj( location.search ).gp || "1";
+    console.log( "game view for gp ... " + gp );
+    vm.gameplayerID = gp;
+    console.log( vm.gameplayerID );
+
+    const url = "http://localhost:8080/api/game_view/" + gp;
+    console.log( url );
+    fetch( url ).then( function(response){
         console.log( "got response: " + response );
         if( response.ok ){
             return response.json();
@@ -10,24 +57,31 @@ function getListOfGames(){
         console.log( "got json: " + json );
          var data = json;
         console.log( data );
-        data.forEach( game => {
-           console.log( game );
-           vm.games.push( game );
-        });
+        data.gameplayers.forEach( gp => {
+            console.log( gp );
+            vm.gameplayers.push( gp );
+         });       
 
 
-    console.log( vm.games );
     }).catch( function( error ){
         console.log( "error: " + error );
     })
 }
 
-getListOfGames();
 
 
-var vm = new Vue({
-    el: '#vue-instance',
-    data: {
-        games: []
-    }
-})
+function paramObj(search) {
+    var obj = {};
+    var reg = /(?:[?&]([^?&#=]+)(?:=([^&#]*))?)(?:#.*)?/g;
+  
+    search.replace(reg, function(match, param, val) {
+      obj[decodeURIComponent(param)] = val === undefined ? "" : decodeURIComponent(val);
+    });
+  
+    return obj;
+  }
+
+
+
+
+//----- plain javascript -----//
